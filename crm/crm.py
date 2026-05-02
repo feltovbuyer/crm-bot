6c1f85dd3f45ce6f2cccc83ed4180f0185254e39
feltovbuyer <artembiznesmenov5@gmail.com>
2026-05-02 23:02:12 +0300

    fix: reduce visible photo previews for faster chat load

--- crm/crm.py
+++ crm/crm.py
@@ -399,7 +399,7 @@ async def show_crm(page: ft.Page):
             )
             # берём только фото
             photo_messages = [m for m in ms if m[4] == "photo"]
-            visible_photo_ids = set(m[0] for m in photo_messages[-25:])
+            visible_photo_ids = set(m[0] for m in photo_messages[-3:])
 
             new_controls = []
 
